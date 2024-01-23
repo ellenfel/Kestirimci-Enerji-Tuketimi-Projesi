9e0c835789b9f155826f1992261eff593a0be86e
ellenfel <ellfelfen@gmail.com>
2024-01-23 18:23:16 +0300

    added bibliography

--- keys.sql
+++ keys.sql
@@ -1,15 +1,40 @@
-WITH LastVeriWithRowNumber AS (
+-- Drop existing tables if they exist
+DROP TABLE IF EXISTS veri;
+DROP TABLE IF EXISTS last_veri;
+
+-- Create the veri table
+CREATE TABLE veri AS (
     SELECT
-        name,
-        TO_TIMESTAMP(ts / 1000) AS formatted_ts,
-        merged_column,
-        ROW_NUMBER() OVER (PARTITION BY name, key ORDER BY ts DESC) AS row_num
-    FROM last_veri
-)
-SELECT
-    name,
-    key,
-    TO_CHAR(formatted_ts, 'DD/MM/YYYY HH24:MI:SS') AS formatted_date,
-    merged_column
-FROM LastVeriWithRowNumber
-WHERE row_num = 1;
+        device.Id,
+        device.name AS devName,
+        ts_kv.ts,
+        ts_kv.key,
+        ts_kv.long_v,
+        ts_kv.dbl_v,
+        ts_kv.str_v,
+        ts_kv.bool_v
+    FROM ts_kv
+    JOIN device ON device.Id = ts_kv.entity_id
+);
+
+ALTER TABLE veri ADD COLUMN merged_column varchar(512); -- Adjust the length as needed
+
+UPDATE veri SET merged_column = CONCAT(bool_v, ' ', long_v, ' ', dbl_v, ' ', str_v);
+
+ALTER TABLE veri DROP COLUMN long_v, DROP COLUMN dbl_v, DROP COLUMN str_v, DROP COLUMN bool_v;
+
+ALTER TABLE veri RENAME COLUMN key TO telemetry;
+
+DROP TABLE IF EXISTS last_veri;
+
+CREATE TABLE last_veri AS (
+    SELECT * FROM veri
+    JOIN ts_kv_dictionary ON veri.telemetry = ts_kv_dictionary.key_id
+);
+
+ALTER TABLE last_veri DROP COLUMN key_id, DROP COLUMN telemetry;
+
+-- Select relevant columns from last_veri with filtering
+SELECT id, devName, key, ts, merged_column
+FROM last_veri
+WHERE devName <> 'UG-67' AND POSITION('error' IN key) = 0;
